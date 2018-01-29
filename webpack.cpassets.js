8c1f5f6ac6f9adad5733b9c516f09287e1f9b213
Sudheer Reddy Kavalakuntla <sudheerdesignintegrator@yahoo.in>
2018-01-29 22:04:19 +0530

    Update webpack.cpassets.js

--- webpack.cpassets.js
+++ webpack.cpassets.js
@@ -2,26 +2,33 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 const PATHS = {
-    src: path.join(__dirname, 'src'),
-    dist: path.join(__dirname, 'dist')
+    src: path.join(__dirname, 'src'), //absolute path to RepoDir/src
+    dist: path.join(__dirname, 'dist') //absolute path to RepoDir/dist
 }
 module.exports = {
     entry: {
+        //Webpack will automatically resolve it to RepoDir/src/js/index.js (if file name is not specified)
+        //main: PATHS.src + '/js/app.js' //Webpack will resolve it to RepoDir/src/js/app.js
         main: PATHS.src + '/js'
     },
     output: {
+        //Will resolve to RepoDir/dist 
         path: PATHS.dist,
+        //[name] is placeholder for entry ie.. main from line `12`
+        //So output file name will be main<somehash>.js
         filename: 'js/[name][hash].js'
     },
     plugins: [
         new CopyWebpackPlugin([
             {
-                from: 'src/assets',
-                to: 'assets'
+                //Note:- No wildcard is specified hence will copy all files and folders
+                from: 'src/assets', //Will resolve to RepoDir/src/assets 
+                to: 'assets' //Copies all files from above dest to dist/assets
             },
             {
-                from: 'src/css/*.css',
-                to: 'css'
+                //Wildcard is specified hence will copy only css files
+                from: 'src/css/*.css', //Will resolve to RepoDir/src/css and all *.css files from this directory
+                to: 'css'//Copies all matched css files from above dest to dist/css
             }
         ])
     ]
